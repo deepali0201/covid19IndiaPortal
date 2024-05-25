d27ba0d2a679c487ff62ae0d30d8059a183c6808
deepali0201 <deepalideepali333@gmail.com>
2024-05-25 17:39:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,18 +55,17 @@ const authenticateToken = (request, response, next) => {
   let jwtToken
   const authHeader = request.headers['authorization']
   if (authHeader !== undefined) {
-    jwtToken = authHeader.split(' ')[1]
+    jwtToken = authHeader.split(" ")[1]
   }
   if (jwtToken === undefined) {
     response.status(401)
     response.send('Invalid JWT Token')
   } else {
-    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+    jwt.verify(jwtToken, 'MY_SECRET_KEY', async (error, payload) => {
       if (error) {
         response.status(401)
         response.send('Invalid JWT Token')
       } else {
-        request.username = payload
         next()
       }
     })
